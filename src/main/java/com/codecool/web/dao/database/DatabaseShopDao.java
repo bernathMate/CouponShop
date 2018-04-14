c44bbc05cc941a74e7cc31027a8246020df014d1
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 17:36:17 +0200

    findAll(), findAllByCouponId(), findById(), add() methods are modified, because shops table is refactored in the database, fetchShop() method is refactored with userId variable

--- src/main/java/com/codecool/web/dao/database/DatabaseShopDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseShopDao.java
@@ -16,7 +16,7 @@ public final class DatabaseShopDao extends AbstractDao implements ShopDao {
     @Override
     public List<Shop> findAll() throws SQLException {
         List<Shop> shops = new ArrayList<>();
-        String sql = "SELECT id, name FROM shops";
+        String sql = "SELECT id, name, user_id FROM shops";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             while (resultSet.next()) {
@@ -29,7 +29,7 @@ public final class DatabaseShopDao extends AbstractDao implements ShopDao {
     @Override
     public List<Shop> findAllByCouponId(int couponId) throws SQLException {
         List<Shop> shops = new ArrayList<>();
-        String sql = "SELECT s.id, s.name " +
+        String sql = "SELECT s.id, s.name, s.user_id " +
             "FROM shops AS s " +
             "JOIN coupons_shops AS cs ON s.id = cs.shop_id " +
             "JOIN coupons AS c ON c.id = cs.coupon_id " +
@@ -47,7 +47,7 @@ public final class DatabaseShopDao extends AbstractDao implements ShopDao {
 
     @Override
     public Shop findById(int id) throws SQLException {
-        String sql = "SELECT id, name FROM shops WHERE id = ?";
+        String sql = "SELECT id, name, user_id FROM shops WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -60,18 +60,19 @@ public final class DatabaseShopDao extends AbstractDao implements ShopDao {
     }
 
     @Override
-    public Shop add(String name) throws SQLException {
+    public Shop add(String name, int userId) throws SQLException {
         if (name == null || "".equals(name)) {
             throw new IllegalArgumentException("Name cannot be null or empty");
         }
         boolean autoCommit = connection.getAutoCommit();
         connection.setAutoCommit(false);
-        String sql = "INSERT INTO shops (name) VALUES (?)";
+        String sql = "INSERT INTO shops (name, user_id) VALUES (?, ?)";
         try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, name);
+            statement.setInt(2, userId);
             executeInsert(statement);
             int id = fetchGeneratedId(statement);
-            return new Shop(id, name);
+            return new Shop(id, name, userId);
         } catch (SQLException ex) {
             connection.rollback();
             throw ex;
@@ -83,6 +84,7 @@ public final class DatabaseShopDao extends AbstractDao implements ShopDao {
     private Shop fetchShop(ResultSet resultSet) throws SQLException {
         int id = resultSet.getInt("id");
         String name = resultSet.getString("name");
-        return new Shop(id, name);
+        int userId = resultSet.getInt("user_id");
+        return new Shop(id, name, userId);
     }
 }
